dd4659ae5cd46f6f03a6e456e0c0eec2794bb1eb
ShakibSDA <shakib@sda-zone.com>
2025-01-09 16:10:52 +0530

    Update UmrahHero.jsx

--- app/umrah/UmrahHero.jsx
+++ app/umrah/UmrahHero.jsx
@@ -4,7 +4,7 @@ const RamdanHero = () => {
   return (
     <main>
       <FloatingDockDemo />
-      <div className="RamadanHero w-full relative z-10 mt-16 !h-[80vh] flex justify-center items-center  text-white/95">
+      <div className="RamadanHero w-full relative z-10 mt-16 !h-[45vh] flex justify-center items-center  text-white/95">
         <p className="font-bold text-7xl ">Umrah 2025</p>
       </div>
     </main>
